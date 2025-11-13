030e0523265ce6db76ec84233a6d48a86bc3d3b3
Benedict254-Ke <lulub8929@gmail.com>
2025-11-13 10:35:36 +0300

    first commit

--- src/admin/utils/api.ts
+++ src/admin/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://victorgraphics.onrender.com/api';
+const API_BASE_URL = 'https://victoe-and-sons.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
